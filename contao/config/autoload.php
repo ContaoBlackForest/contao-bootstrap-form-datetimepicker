f0c59836a5f357ea19fb2b53cfe73ef4c633e368
Sven Baumann <baumann.sv@gmail.com>
2015-09-22 01:54:44 +0200

    register template for autoload

---
+++ contao/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Bootstrap datetimepicker for forms in Contao CMS
+ *
+ * Copyright (C) Bootstrap datetimepicker for forms in Contao CMS
+ *
+ * @package   contao-bootstrap-form-datetimepicker
+ * @file      autoload.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2015 ContaoBlackForest
+ */
+
+
+\TemplateLoader::addFiles(
+    array(
+        'form_bootstrap_calendar_field' => 'system/modules/contao-bootstrap-form-datetimepicker/templates/forms',
+    )
+);
